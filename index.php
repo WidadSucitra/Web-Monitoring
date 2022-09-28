997aae0c1553faf6b465f748eac32bb403ec078d
AlifahDila19 <79298662+AlifahDila19@users.noreply.github.com>
2022-09-28 15:06:21 +0800

    pembuatan form

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+include "navbar.php";
+include "config.php";
+?>
+
+
+    <section class="laporan-progres-pendataan">
+        <div class="laporan p">
+            <label for="nama">Nama</label>
+            <input type="nama" name="nama" id="">
+        </div>
+        <div class="laporan-progres-pendataan">
+            <label for="kode petugas">Kode Petugas</label>
+            <input type="kode petugas" name="kode petugas" id="">
+        </div>
+        <div class="laporan-progres-pendataan">
+            <label for="kode wilayah">Kode Wilayah</label>
+            <input type="kode wilayah" name="kode wilayah" id="">
+        </div>
+        <div class="laporan-progres-pendataan">
+            <label for="desa">Desa</label>
+            <input type="desa" name="desa" id="">
+        </div>
+        <div class="laporan-progres-pendataan">
+            <label for="kecamatan">Kecamatan</label>
+            <input type="kecamatan" name="kecamatan" id="">
+        </div>
+        <div class="laporan-progres-pendataan">
+            <label for="tanggal">Tanggal</label>
+            <input type="tanggal" name="tanggal" id="">
+        </div>
+        <div class="laporan-progres-pendataan">
+            <label for="progress">Progress</label>
+            <input type="progress" name="progress" id="">
+        </div>
+    </section>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.6/dist/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  </body>
+</html>

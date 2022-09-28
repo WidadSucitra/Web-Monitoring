2b04aebc5db21abe3bb38c293e7111ae1f12e455
Andi WIdad Sucitra <sucitraandiwidad@gmail.com>
2022-09-28 13:25:54 +0800

    perbaika

--- login.php
+++ login.php
@@ -1,22 +1,7 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-
-    <title>Monitoring</title>
-  </head>
-  <body>
-    <nav class="navbar">
-        <img class="logo-bps" src="img/logo.png" alt="">
-        <h1  class="brand-bps"> <i>BADAN PUSAT STATISTIK <br>KABUPATEN GOWA</i></h1> 
-    </nav>      
-    
+<?php
+include "navbar.php";
+include "config.php";
+?>
     <section class="login-admin">
         <div class="container">
             <div class="row">
